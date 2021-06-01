a23615262c297afd1667d728db5a6396535ff826
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-01 14:43:33 -0400

    let players take and draw cards

--- lib/war_player.rb
+++ lib/war_player.rb
@@ -8,10 +8,12 @@
      return @player_deck.cards_left
    end
 
-   def take_card
+   def take_card(new_card)
+     @player_deck.add_card(new_card)
    end
 
    def draw_card
+     @player_deck.draw
    end
 
  end
--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -6,4 +6,16 @@ describe 'WarPlayer' do
   it('starts without any cards') do
     expect(player.card_count).to eq(0)
   end
+
+  it('takes a card and adds it to the bottom of the deck') do
+    player.take_card(PlayingCard.new("2", "H"))
+    player.take_card(PlayingCard.new("3", "H"))
+    expect(player.draw_card).to eq(PlayingCard.new("2", "H"))
+  end
+
+  it('removes the card at the top of the deck and returns it') do
+    drawn_card = player.draw_card
+    player.take_card(PlayingCard.new("4", "H"))
+    expect(player.draw_card).to eq(PlayingCard.new("4", "H"))
+  end
 end
